d09bd639ea738cb8887fe36dbfc38daf4cfabbf0
Bengg04 <benjamin04.meier@bluewin.ch>
2023-06-27 19:18:03 +0200

    added js and loaded the first dropdown

--- js/Stundenplan.js
+++ js/Stundenplan.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
-    
+
+    let groupDropdown = document.getElementById("groupDropdown");
+
+    $.getJSON("http://sandbox.gibm.ch/berufe.php").done(function (data) {
+        console.log(data)
+        $.each(data, function (index, group) {
+            let option = "<option value='" + group.beruf_id + "'>" + group.beruf_name + "</option>"
+            groupDropdown.insertAdjacentHTML("beforeend", option)
+        })
+    })
 })
