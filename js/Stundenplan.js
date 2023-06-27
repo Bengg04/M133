10b4c349ed377bc17c7abd699d5ecf9417e77ba0
Bengg04 <benjamin04.meier@bluewin.ch>
2023-06-27 23:33:33 +0200

    added output

--- js/Stundenplan.js
+++ js/Stundenplan.js
@@ -23,9 +23,17 @@ $(document).ready(function () {
 
     classDropdown.addEventListener("change", function () {
         let selectedClass = classDropdown.value;
+        let  wochentag = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"];
+        let tableHead = "<table><tr><th>Datum</th><th>Wochentag</th><th>Von</th><th>Bis</th><th>Lehrer</th><th>Fach</th><th>Raum</th></tr>"
 
         $.getJSON("http://sandbox.gibm.ch/tafel.php?klasse_id=" + selectedClass).done(function (data) {
-            console.log(data);
+            $.each(data, function (index, lession) {
+                let week = "<tr><td>" + lession.tafel_datum + "</td><td>" + wochentag[lession.tafel_wochentag] +"</td><td>" + lession.tafel_von +"</td><td>" + lession.tafel_bis +"</td><td>" + lession.tafel_lehrer +"</td><td>" + lession.tafel_longfach +"</td><td>" + lession.tafel_raum +"</td></tr>"
+                tableHead += week;
+            })
+            tableHead += "</table>"
+            document.getElementById("outputDiv").innerHTML = tableHead
         })
+
     })
 })
